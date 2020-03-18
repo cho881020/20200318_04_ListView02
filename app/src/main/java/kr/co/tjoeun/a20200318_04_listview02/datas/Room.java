e16f4e83328b5db654818e8870c538203af848e3
cho881020 <kj_cho@nepp.kr>
2020-03-18 17:51:11 +0900

    방 상세 화면 데이터 출력  - Room클래스의 기능으로 층수 가공 추가.  - Adapter / 방상세화면에서는 가져다 사용만.

--- app/src/main/java/kr/co/tjoeun/a20200318_04_listview02/datas/Room.java
+++ app/src/main/java/kr/co/tjoeun/a20200318_04_listview02/datas/Room.java
@@ -9,6 +9,28 @@ public class Room implements Serializable {
     private int floor;
     private String description;
 
+    public String getFloorToString() {
+//        층수를 => 상황에 맞는 String 으로 가공.
+
+        String floorStr = "";
+//        층 > 0, 층 == 0, 그외 (층 < 0)
+
+        if (this.floor > 0) {
+//            4 => "4층" 으로 가공
+            floorStr = String.format("%d층", this.floor);
+        }
+        else if (this.floor == 0) {
+//            0 => "반지하" 로.
+            floorStr = "반지하";
+        }
+        else {
+//            -1 => "지하 1층" 으로 가공
+            floorStr = String.format("지하 %d층", this.floor*-1);
+        }
+
+        return floorStr;
+    }
+
     public String getFormattedPrice() {
 //        상황에 따라 ?억 ?천,  ?천인지로.
 //        나눠서 리턴을 해줌.
