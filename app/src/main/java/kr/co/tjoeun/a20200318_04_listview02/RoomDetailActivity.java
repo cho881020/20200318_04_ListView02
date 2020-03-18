5835adb281e6be1cc767c9a4466bb0c026a7411b
cho881020 <kj_cho@nepp.kr>
2020-03-18 17:44:48 +0900

    방 상세 화면 데이터 출력  - Room클래스의 기능으로 가격 가공 추가.  - Adapter / 방상세화면에서는 가져다 사용만.

--- app/src/main/java/kr/co/tjoeun/a20200318_04_listview02/RoomDetailActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200318_04_listview02/RoomDetailActivity.java
@@ -31,6 +31,9 @@ public class RoomDetailActivity extends BaseActivity {
 //        첨부된 방 데이터를 받아서 화면에 출력.
         Room room = (Room) getIntent().getSerializableExtra("room");
 
+        binding.priceTxt.setText(room.getFormattedPrice());
+
+        binding.descTxt.setText(room.getDescription());
 
 
     }
